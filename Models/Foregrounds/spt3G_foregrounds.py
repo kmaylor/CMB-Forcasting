6883c04d882d5068bfff664d01ac8d91fa5b7db4
kmaylor <kmaylor@ucdavis.edu>
2017-08-30 15:57:59 -0700

    updated SPT3g foregrounds

--- Models/Foregrounds/spt3G_foregrounds.py
+++ Models/Foregrounds/spt3G_foregrounds.py
@@ -1,5 +1,5 @@
 
-from numpy import hstack, loadtxt, arange, errstate
+from numpy import hstack, loadtxt, arange, errstate, zeros
 
 class foregrounds():
     '''
@@ -15,17 +15,17 @@ class foregrounds():
                  Asz=5.5,
                  Aps=19.3,
                  Acib=5,
-                 A_TE80 = 2,
-                 A_EE80 = 2,
-                 a_TE = -2.42,
-                 a_EE = -2.42,
-                 D_PSEE_3000 = 2.5,
+                 A_TEps = .16,
+                 #A_EE80 = 2,
+                 #a_TE = -2.42,
+                 #a_EE = -2.42,
+                 #D_PSEE_3000 = 2.5,
                  **kwargs):
     
             ell = arange(len(self.sz_template))
             TT_foregrounds = Asz * self.sz_template + Aps * self.poisson_template + Acib * self.cluster_template
             with errstate(divide='ignore'):
-                EE_foregrounds = A_EE80*(ell/80.)**(a_EE+2) + D_PSEE_3000*(ell/3000.)**2
-                TE_foregrounds = A_TE80*(ell/80.)**(a_TE+2)
+                EE_foregrounds = zeros(len(ell))#A_EE80*(ell/80.)**(a_EE+2) + D_PSEE_3000*(ell/3000.)**2
+                TE_foregrounds = A_TEps*(ell/3000)**2
             
             return {'TT':TT_foregrounds, 'EE': EE_foregrounds, 'TE':TE_foregrounds}
